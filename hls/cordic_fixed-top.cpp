4671cc7a8c5bc0ee56fb88face2b9086a1b76e3e
Po-Yen Chu <55480884+poyenchu@users.noreply.github.com>
2020-12-25 21:27:09 +0800

    Add files via upload

---
+++ hls/cordic_fixed-top.cpp
@@ -0,0 +1,72 @@
+/*
+This is traditional CORDIC computation of sine and cosine.
+The current code is based on [FXT: cordic-circ-demo.cc]
+Correctly calculates cos and sine between 0-90 degrees (0-100).
+
+INPUT:
+	double theta: Input angle 
+	long n: Number of iterations. 
+OUTPUT:
+	double &s: Reference to the sine part
+	double &c: Reference to the cos part 
+
+	error_sin= [abs(s-zs)/zs]*100;
+	error_cos= [abs(c-zc)/zc]*100;
+	Total_Error_Sin = sum(error_sin)
+	Total_error_Cos = sum(error_cos)
+
+*/
+#include <math.h>
+#include"cordic.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+//#define M_PI 3.1415926536897932384626
+
+double abs_double(double var){
+	if ( var < 0)
+	var = -var;
+	return var;
+
+
+}
+int main(int argc, char **argv)
+{	
+
+
+    FILE *fp;
+
+	COS_SIN_TYPE s;			//sine
+	COS_SIN_TYPE c;			//cos
+	THETA_TYPE radian; 		//radian versuin of degree
+
+	//zs=sin, zc=cos using math.h in VivadoHLS
+	double zs, zc; 			// sine and cos values calculated from math.
+
+	//Error checking
+	double Total_Error_Sin=0.0;
+	double Total_error_Cos=0.0;
+	double error_sin=0.0, error_cos=0.0;
+
+	fp=fopen("out.dat","w");
+	for(int i=10;i<NUM_DEGREE;i=i+10) {
+			radian = i*M_PI/180;
+			cordic(radian, s, c);
+			zs = sin((double)radian);
+			zc = cos((double)radian);
+			error_sin=(abs_double((double)s-zs)/zs)*100.0;
+			error_cos=(abs_double((double)c-zc)/zc)*100.0;
+			Total_Error_Sin=Total_Error_Sin+error_sin;
+			Total_error_Cos=Total_error_Cos+error_cos;
+			printf("design: degree=%d, radian=%f, cos=%f, sin=%f\n", i, (double)radian, (double)c, (double)s);
+			printf("golden: degree=%d, radian=%f, cos=%f, sin=%f\n", i, (double)radian, (double)zc, (double)zs);
+			//fprintf(fp, "design: degree=%d, radian=%f, cos=%f, sin=%f\n", i, (double)radian, (double)c, (double)s);
+			//fprintf(fp, "golden: degree=%d, radian=%f, cos=%f, sin=%f\n", i, (double)radian, (double)zc, (double)zs);
+	}
+
+	fclose(fp);
+
+	// printf ("Total_Error_Sin=%f, Total_error_Cos=%f, \n", Total_Error_Sin, Total_error_Cos);
+	return 0;
+}
---
+++ hls/directives.tcl
@@ -0,0 +1,9 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_interface -mode s_axilite "cordic"
+set_directive_interface -mode s_axilite "cordic" theta
+set_directive_interface -mode s_axilite "cordic" s
+set_directive_interface -mode s_axilite "cordic" c
